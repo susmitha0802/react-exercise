0e31f635a774a700b025f2070e3e1b1499e977bc
susmitha0802 <susmitha0802@gmail.com>
2023-09-27 13:12:30 +0530

    SignUp Form updated

--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -1,3 +1,7 @@
+import { SwitchProvider } from './SwitchContext';
+import Switch from './Switch';
+import Form from './Form';
+
 export default function SignUpForm({formDetails, valid, onSubmit, onChange}) {
     return (
         <>
@@ -5,59 +9,13 @@ export default function SignUpForm({formDetails, valid, onSubmit, onChange}) {
                 <h1>Thank you!!</h1> :
                 <div>
                     <h2>SignUp Form</h2>
-                    <form onSubmit={onSubmit}>
-                        <div>
-                            <label> Name: <input 
-                                type="text"
-                                name="name"
-                                value={formDetails.name}
-                                placeholder="Enter your name"
-                                onChange = {onChange} 
-                                required/>
-                            </label>
-                        </div>
-                        <br/>
-                        <div>
-                            <label> Email: <input 
-                                type="email"
-                                name="email" 
-                                value={formDetails.email}
-                                placeholder="Enter your email"
-                                onChange = {onChange}
-                                required/>
-                            </label>
-                            <div id="error-email"></div>
-                        </div>
-                        <br/>
-                        <div>
-                            <label> Password: <input 
-                                type="password" 
-                                name="password"
-                                value={formDetails.password}
-                                placeholder="Enter your password"
-                                onChange = {onChange}
-                                required/>
-                            </label>
-                        </div>
-                        <br/>
-                        <div>
-                            <label> Confirm Password: <input 
-                                type="password" 
-                                name="confirmPassword"
-                                value={formDetails.confirmPassword}
-                                placeholder="Confirm your password"
-                                onChange = {onChange}
-                                required/>
-                            </label>
-                            <div id="error-confirmPassword"></div>
-                        </div>
-                        <br/>
-                        <div>
-                            <input type="submit"/>
-                        </div>
-                    </form>
+                    <SwitchProvider>
+                        <Switch/>
+                        <Form formDetails={formDetails} onSubmit={onSubmit} onChange={onChange}/>
+                    </SwitchProvider>
                 </div>  
             } 
         </>
     );
 }
+
